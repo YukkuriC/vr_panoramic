7b90e5fbf349a3f988101a692c04e92a51cec732
Yukkuri C <799433638@qq.com>
2018-10-23 00:10:24 +0800

    视频间平滑切换功能

--- Assets/Solution1/VideoSwapper.cs
+++ Assets/Solution1/VideoSwapper.cs
@@ -11,39 +11,36 @@ public class VideoSwapper : MonoBehaviour {
 	public CanvasGroup shader;
 
 	int ptr=0;
+	bool isSwapping=false;
 
 	const int split=10;
 	const float lag=0.01f;
 
-	IEnumerator FadeIn(){
+	IEnumerator FadeIn(int to){
 		for(int i=split;i>=0;i--){
+			vps[to].SetDirectAudioVolume(0,1-(float)i/split);
 			shader.alpha=(float)i/split;
-			yield  return new  WaitForSeconds(lag);
+			yield return new WaitForSeconds(lag);
 		}
 	}
 
-	IEnumerator FadeOut(int to){
-		for(int i=0;i<=split;i++){
-			shader.alpha=(float)i/split;
-			yield  return new  WaitForSeconds(lag);
-		}
-		output.SetTexture("_MainTex",vts[to]);
-		yield  return FadeIn();
-	}
 	IEnumerator Switch(int from,int to){
-		vps[to].volumn=0;
+		Debug.Log(from);
+		isSwapping=true;
+		vps[to].SetDirectAudioVolume(0,0);
 		vps[to].frame=vps[ptr].frame;
 		vps[to].Play();
+		Debug.Log(to);
 		for(int i=0;i<=split;i++){
-			vps[from].volumn=shader.alpha=(float)i/split;
-			yield  return new  WaitForSeconds(lag);
+			vps[from].SetDirectAudioVolume(0,1-(float)i/split);
+			shader.alpha=(float)i/split;
+			yield return new WaitForSeconds(lag);
 		}
 		vps[ptr].Pause();
 		output.SetTexture("_MainTex",vts[to]);
-		for(int i=split;i>=0;i--){
-			vps[to].volumn=shader.alpha=(float)i/split;
-			yield  return new  WaitForSeconds(lag);
-		}
+		ptr=to;
+		yield return FadeIn(to);
+		isSwapping=false;
 	}
 
 	void Start(){
@@ -52,34 +49,22 @@ public class VideoSwapper : MonoBehaviour {
 			vps[i].targetTexture=vts[i];
 		}
 		ptr=0;
+		isSwapping=false;
 		vps[0].Play();
-		StartCoroutine(FadeIn());
+		output.SetTexture("_MainTex",vts[0]);
+		StartCoroutine(FadeIn(0));
 	}
 
-	void SwitchVideo (int to) {
-		StartCoroutine(FadeOut(to));
-		// Debug.Log(to);
-		if(vps[ptr].isPlaying){
-			vps[to].frame=vps[ptr].frame;
-			vps[ptr].Pause();
-			vps[to].Play();
-		}else{
-
-		}
-		// output.SetTexture("_MainTex",vts[to]);
-		ptr=to;
-	}
-	
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Space)){
 			if(vps[ptr].isPlaying)vps[ptr].Pause();
 			else vps[ptr].Play();
 		}
-		if(Input.GetKeyDown(KeyCode.LeftArrow))
-			// SwitchVideo((ptr+2)%3);
-			Switch(ptr,(ptr+vps.Length-1)%vps.Length);
-		if(Input.GetKeyDown(KeyCode.RightArrow))
-			// SwitchVideo((ptr+1)%3);
-			Switch(ptr,(ptr+1)%vps.Length);
+		if(!isSwapping){
+			if(Input.GetKeyDown(KeyCode.LeftArrow))
+				StartCoroutine(Switch(ptr,(ptr+vps.Length-1)%vps.Length));
+			if(Input.GetKeyDown(KeyCode.RightArrow))
+				StartCoroutine(Switch(ptr,(ptr+1)%vps.Length));
+		}
 	}
 }
