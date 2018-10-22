23e268f211462bf2e0d52aba7452eb4d4799c68e
YukkuriC <799433638@qq.com>
2018-10-22 16:18:40 +0800

    移除solution2; 传输未完成代码

--- Assets/Solution1/VideoSwapper.cs
+++ Assets/Solution1/VideoSwapper.cs
@@ -8,27 +8,65 @@ public class VideoSwapper : MonoBehaviour {
 	public VideoPlayer[] vps;
 	public RenderTexture[] vts;
 	public Material output;
+	public CanvasGroup shader;
 
 	int ptr=0;
 
+	const int split=10;
+	const float lag=0.01f;
+
+	IEnumerator FadeIn(){
+		for(int i=split;i>=0;i--){
+			shader.alpha=(float)i/split;
+			yield  return new  WaitForSeconds(lag);
+		}
+	}
+
+	IEnumerator FadeOut(int to){
+		for(int i=0;i<=split;i++){
+			shader.alpha=(float)i/split;
+			yield  return new  WaitForSeconds(lag);
+		}
+		output.SetTexture("_MainTex",vts[to]);
+		yield  return FadeIn();
+	}
+	IEnumerator Switch(int from,int to){
+		vps[to].volumn=0;
+		vps[to].frame=vps[ptr].frame;
+		vps[to].Play();
+		for(int i=0;i<=split;i++){
+			vps[from].volumn=shader.alpha=(float)i/split;
+			yield  return new  WaitForSeconds(lag);
+		}
+		vps[ptr].Pause();
+		output.SetTexture("_MainTex",vts[to]);
+		for(int i=split;i>=0;i--){
+			vps[to].volumn=shader.alpha=(float)i/split;
+			yield  return new  WaitForSeconds(lag);
+		}
+	}
+
 	void Start(){
-		SwitchVideo(0);
 		for(int i=0;i<vps.Length;i++){
 			vps[i].Prepare();
 			vps[i].targetTexture=vts[i];
 		}
-		// for(int i=0;i<vps.Length;i++)vps[i].targetTexture=vts[i];
+		ptr=0;
+		vps[0].Play();
+		StartCoroutine(FadeIn());
 	}
 
 	void SwitchVideo (int to) {
+		StartCoroutine(FadeOut(to));
+		// Debug.Log(to);
 		if(vps[ptr].isPlaying){
 			vps[to].frame=vps[ptr].frame;
-			vps[to].Play();
 			vps[ptr].Pause();
+			vps[to].Play();
 		}else{
 
 		}
-		output.SetTexture("_MainTex",vts[to]);
+		// output.SetTexture("_MainTex",vts[to]);
 		ptr=to;
 	}
 	
@@ -38,8 +76,10 @@ public class VideoSwapper : MonoBehaviour {
 			else vps[ptr].Play();
 		}
 		if(Input.GetKeyDown(KeyCode.LeftArrow))
-			SwitchVideo((ptr+2)%3);
+			// SwitchVideo((ptr+2)%3);
+			Switch(ptr,(ptr+vps.Length-1)%vps.Length);
 		if(Input.GetKeyDown(KeyCode.RightArrow))
-			SwitchVideo((ptr+1)%3);
+			// SwitchVideo((ptr+1)%3);
+			Switch(ptr,(ptr+1)%vps.Length);
 	}
 }
